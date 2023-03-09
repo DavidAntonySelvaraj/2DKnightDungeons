f78da0d0ce8b9a5b49ccfb0ee7eb2d2a49e9c610
DavidAntonySelvaraj <davidselvaraj1234@gmail.com>
2023-03-10 04:42:32 +0530

    .

--- KnightCrash/Assets/Prototype/PlayerMovement.cs
+++ KnightCrash/Assets/Prototype/PlayerMovement.cs
@@ -4,39 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField]
-    private float moveSpeed;
-
-    private Rigidbody2D playerRb;
-
-
-    private void Awake()
-    {
-        playerRb = GetComponent<Rigidbody2D>();
-    }
-
-    private void FixedUpdate()
-    {
-        movePlayer();
-    }
-
-
-    void movePlayer()
-    {
-
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.position += new Vector3(transform.position.x + moveSpeed*Time.deltaTime, transform.position.y, transform.position.z);
-
-            
-        }
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-          //  playerRb.velocity = new Vector2(playerRb.velocity.x + moveSpeed * Time.deltaTime, 0);
-        }
-    }
-
-   
+    
 
 }//class
 
