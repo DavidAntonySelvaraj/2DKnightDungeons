b76ada44e93619f7165e7799da43cfed6401d5d4
DavidAntonySelvaraj <davidselvaraj1234@gmail.com>
2023-03-10 22:07:14 +0530

    .

--- KnightCrash/Assets/Prototype/PlayerMovement.cs
+++ KnightCrash/Assets/Prototype/PlayerMovement.cs
@@ -4,7 +4,36 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float radius;
+
+    private Transform playerTransform;
+
+    private bool canMove;
+
+    private void Awake()
+    {
+        playerTransform = GetComponent<Transform>();
+    }
+    private void FixedUpdate()
+    {
+        CanMovePlayer();
+    }
+    void CanMovePlayer()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(playerTransform.position, transform.right, radius, 3 << LayerMask.NameToLayer("Wall"));
+
+        //Debug.DrawRay(transform.position, transform.right, Color.white);
+
+        if (hit)
+        {
+            Debug.Log("Its Hit");
+        }
+        else
+        {
+            Debug.Log("Not hit");
+        }
+    }
 
 }//class
 
